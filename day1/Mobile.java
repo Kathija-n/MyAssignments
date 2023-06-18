342f0392324f36d66ae88f035d021844c244cb49
Kathija-n <136942534+Kathija-n@users.noreply.github.com>
2023-06-18 07:38:09 +0530

    Add files via upload
    Week1day1

---
+++ day1/Mobile.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class Mobile {
+
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		String bodyColour="Red";
+		String IPaddress="10.35.24.248";
+		String brand="Apple";
+		boolean Ischarged=true;
+		 float milage=8.00f;
+		 long IMEI=846977568245212l;
+		 double Uptime=26.29;
+		 System.out.println(bodyColour);
+	        System.out.println(brand);
+	        System.out.println(Ischarged);
+	        System.out.println(milage);
+	        System.out.println(IMEI);
+	        System.out.println(Uptime);
+	        System.out.println(IPaddress);
+	}
+
+}
